813a9be7182930202efdac3097f1fb97411b52a9
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-09-11 14:21:59 +0700

    feat: add content in experience home page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
 const experienceListDesc = [
-  '[Long paragraph my experience in PT.Netta Code Indonesia]',
-  '[Long paragraph my experience in Catamyst x Codepolitan]',
-  '[Long paragraph my experience in Tanjungpura University]',
-  '[Long paragraph my experience in Bersihberes]',
+  'I am working in PT. Nettacode Indonesia as a mobile developer since September 2021 until now. I am developing an android app named PIFA and publish it in playstore. I developed that app using dart programming language and flutter SDK.',
+  'I\'m taking the catamyst x codepolitan front-end bootcamp class from august 2022 until october 2022. In this bootcamp I learn front-end web development from zero. I learn how to create markdown, design UI in Figma, and learn how to develop a website using HTML, CSS, javascript, and React.',
+  'I am an undergraduate student at Tanjungpura Unversity majoring in computer system engineering since July 2016 and will be finished on October 2022. when I was in college I focused on studying mobile and web development. My final project research made a website to detect clickbait news from news titles using Multinomial Naive Bayes algorithm.',
+  'I am working in Bersihberes as a mobile application developer since July 2020 until January 2021. I am developing an android app named PIFA and publish it in playstore. I developed that app using dart programming language and flutter SDK.',
 ]
 let indexExperienceChoose = 0;
 
